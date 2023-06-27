bf24777adadde5b557446fdf5c5c0c40a6c7791b
Mani <manichandra.teja@gmail.com>
2023-06-27 08:37:31 +0530

    initial commit setup

---
+++ bots-deployment-detector/src/agent.ts
@@ -0,0 +1,43 @@
+import {
+  BlockEvent,
+  Finding,
+  Initialize,
+  HandleBlock,
+  HandleTransaction,
+  HandleAlert,
+  AlertEvent,
+  TransactionEvent,
+  FindingSeverity,
+  FindingType,
+} from 'forta-agent';
+
+let findingsCount = 0;
+
+const handleTransaction: HandleTransaction = async (txEvent: TransactionEvent) => {
+  const findings: Finding[] = [];
+
+  return findings;
+};
+
+// const initialize: Initialize = async () => {
+//   // do some initialization on startup e.g. fetch data
+// }
+
+// const handleBlock: HandleBlock = async (blockEvent: BlockEvent) => {
+//   const findings: Finding[] = [];
+//   // detect some block condition
+//   return findings;
+// }
+
+// const handleAlert: HandleAlert = async (alertEvent: AlertEvent) => {
+//   const findings: Finding[] = [];
+//   // detect some alert condition
+//   return findings;
+// }
+
+export default {
+  // initialize,
+  handleTransaction,
+  // handleBlock,
+  // handleAlert
+};
