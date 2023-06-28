81cfb933288dd063db0c05fcca36ba201f04dbaf
Mani <manichandra.teja@gmail.com>
2023-06-28 16:06:49 +0530

    tests: removed nested describe blocks and refactored the tests

--- bots-deployment-detector/src/agent.spec.ts
+++ bots-deployment-detector/src/agent.spec.ts
@@ -16,142 +16,138 @@ describe('Bot Deployment Detector Agent', () => {
 
   const mockAgentId = '34567';
   const mockChainId = 137;
-  const eventTypes = ['AgentCreated', 'AgentUpdated', 'AgentEnabled', 'AgentDisabled'];
 
   beforeAll(() => {
     handleTransaction = provideHandleTransaction(mockDeployerAdddress, mockContractAddress);
   });
 
   describe('handleTransaction', () => {
-    describe('Ignores Transaction', () => {
-      it('ignores any transaction that is not a from deployer Address', async () => {
-        const txEvent = new TestTransactionEvent();
-        const findings = await handleTransaction(txEvent);
+    it('ignores any transaction that is not a from deployer Address', async () => {
+      const txEvent = new TestTransactionEvent();
+      const findings = await handleTransaction(txEvent);
 
-        expect(findings).toStrictEqual([]);
-      });
-
-      it('ignores a transaction that is from a deployer Address but not a bot deployment', async () => {
-        const txEvent = new TestTransactionEvent();
-        txEvent.setFrom(mockDeployerAdddress).setTo(mockRandomAddress).setValue('100000');
-
-        const findings = await handleTransaction(txEvent);
-
-        expect(findings).toStrictEqual([]);
-      });
-
-      it('ignores a transaction that is from a deployer Address and bot deployment but to a different bot registry/contract', async () => {
-        const txEvent = new TestTransactionEvent();
-        txEvent.setFrom(mockDeployerAdddress).setTo(mockRandomAddress);
-
-        txEvent.addTraces({
-          function: functionCreateAgent,
-          from: mockDeployerAdddress,
-          to: mockRandomAddress,
-          arguments: [mockAgentId, mockDeployerAdddress, '', [mockChainId]],
-        });
-
-        const findings = await handleTransaction(txEvent);
-
-        expect(findings).toStrictEqual([]);
-      });
+      expect(findings).toStrictEqual([]);
     });
 
-    describe('Returns a Finding', () => {
-      let txEvent: TestTransactionEvent;
+    it('ignores a transaction that is from a deployer Address but not a bot deployment', async () => {
+      const txEvent = new TestTransactionEvent();
+      txEvent.setFrom(mockDeployerAdddress).setTo(mockRandomAddress).setValue('100000');
 
-      const updateInputs = [mockAgentId, mockDeployerAdddress, '', [mockChainId]];
-      const enableInputs = [mockAgentId, true, 1, true];
-      const disableInputs = [mockAgentId, false, 1, false];
+      const findings = await handleTransaction(txEvent);
 
-      const findingType = {
-        severity: FindingSeverity.Info,
-        type: FindingType.Info,
-      };
+      expect(findings).toStrictEqual([]);
+    });
 
-      beforeEach(() => {
-        txEvent = new TestTransactionEvent();
-        txEvent.setFrom(mockDeployerAdddress).setTo(mockContractAddress);
+    it('ignores a transaction that is from a deployer Address and bot deployment but to a different bot registry/contract', async () => {
+      const txEvent = new TestTransactionEvent();
+      txEvent.setFrom(mockDeployerAdddress).setTo(mockRandomAddress);
+
+      txEvent.addTraces({
+        function: functionCreateAgent,
+        from: mockDeployerAdddress,
+        to: mockRandomAddress,
+        arguments: [mockAgentId, mockDeployerAdddress, '', [mockChainId]],
       });
 
-      it('returns a finding with metadata.type: "AgentUpdated" when the transaction is from deployer to update/upgrade of a bot', async () => {
-        txEvent.addEventLog(eventAgentUpdated, mockContractAddress, updateInputs);
+      const findings = await handleTransaction(txEvent);
 
-        const findings = await handleTransaction(txEvent);
+      expect(findings).toStrictEqual([]);
+    });
 
-        expect(findings).toStrictEqual([
-          Finding.fromObject({
-            ...findingAgentInputs.update,
-            ...findingType,
-            metadata: {
-              agentId: mockAgentId,
-              by: mockDeployerAdddress,
-              chainIds: mockChainId.toString(),
-            },
-          }),
-        ]);
+    // Setup for returns a finding.
+    let txEvent: TestTransactionEvent;
+
+    const updateInputs = [mockAgentId, mockDeployerAdddress, '', [mockChainId]];
+    const enableInputs = [mockAgentId, true, 1, true];
+    const disableInputs = [mockAgentId, false, 1, false];
+
+    const findingType = {
+      severity: FindingSeverity.Info,
+      type: FindingType.Info,
+    };
+
+    beforeEach(() => {
+      txEvent = new TestTransactionEvent();
+      txEvent.setFrom(mockDeployerAdddress).setTo(mockContractAddress);
+    });
+
+    it('returns a finding with metadata.type: "AgentUpdated" when the transaction is from deployer to update/upgrade of a bot', async () => {
+      txEvent.addEventLog(eventAgentUpdated, mockContractAddress, updateInputs);
+
+      const findings = await handleTransaction(txEvent);
+
+      expect(findings).toStrictEqual([
+        Finding.fromObject({
+          ...findingAgentInputs.update,
+          ...findingType,
+          metadata: {
+            agentId: mockAgentId,
+            by: mockDeployerAdddress,
+            chainIds: mockChainId.toString(),
+          },
+        }),
+      ]);
+    });
+
+    it('returns a finding with metadata.type: "AgentCreated" when transaction is from deployer for the deployment of the bot', async () => {
+      txEvent.addEventLog(eventAgentUpdated, mockContractAddress, updateInputs);
+
+      txEvent.addTraces({
+        function: functionCreateAgent,
+        arguments: [mockAgentId, mockDeployerAdddress, '', [mockChainId]],
+        from: mockDeployerAdddress,
+        to: mockContractAddress,
       });
 
-      it('returns a finding with metadata.type: "AgentCreated" when transaction is from deployer for the deployment of the bot', async () => {
-        txEvent.addEventLog(eventAgentUpdated, mockContractAddress, updateInputs);
+      const findings = await handleTransaction(txEvent);
 
-        txEvent.addTraces({
-          function: functionCreateAgent,
-          arguments: [mockAgentId, mockDeployerAdddress, '', [mockChainId]],
-          from: mockDeployerAdddress,
-          to: mockContractAddress,
-        });
+      expect(findings).toStrictEqual([
+        Finding.fromObject({
+          ...findingAgentInputs.create,
+          ...findingType,
+          metadata: {
+            agentId: mockAgentId,
+            by: mockDeployerAdddress,
+            chainIds: mockChainId.toString(),
+          },
+        }),
+      ]);
+    });
 
-        const findings = await handleTransaction(txEvent);
+    it('returns a finding with metadata.type: "AgentEnabled" when the transaction is from deployer for enabling the Agent', async () => {
+      txEvent.addEventLog(eventAgentEnabled, mockContractAddress, enableInputs);
 
-        expect(findings).toStrictEqual([
-          Finding.fromObject({
-            ...findingAgentInputs.create,
-            ...findingType,
-            metadata: {
-              agentId: mockAgentId,
-              by: mockDeployerAdddress,
-              chainIds: mockChainId.toString(),
-            },
-          }),
-        ]);
-      });
+      const findings = await handleTransaction(txEvent);
 
-      it('returns a finding with metadata.type: "AgentEnabled" when the transaction is from deployer for enabling the Agent', async () => {
-        txEvent.addEventLog(eventAgentEnabled, mockContractAddress, enableInputs);
+      expect(findings).toEqual([
+        Finding.fromObject({
+          ...findingAgentInputs.enable,
+          ...findingType,
+          metadata: {
+            agentId: mockAgentId,
+            enabled: 'true',
+            permission: '1',
+          },
+        }),
+      ]);
+    });
 
-        const findings = await handleTransaction(txEvent);
+    it('returns a finding with metadata.type: "AgentDisbaled" when the transaction is from deployer for disabling the Agent', async () => {
+      txEvent.addEventLog(eventAgentEnabled, mockContractAddress, disableInputs);
 
-        expect(findings).toEqual([
-          Finding.fromObject({
-            ...findingAgentInputs.enable,
-            ...findingType,
-            metadata: {
-              agentId: mockAgentId,
-              enabled: 'true',
-              permission: '1',
-            },
-          }),
-        ]);
-      });
+      const findings = await handleTransaction(txEvent);
 
-      it('returns a finding with metadata.type: "AgentDisbaled" when the transaction is from deployer for disabling the Agent', async () => {
-        txEvent.addEventLog(eventAgentEnabled, mockContractAddress, disableInputs);
-
-        const findings = await handleTransaction(txEvent);
-
-        expect(findings).toEqual([
-          Finding.fromObject({
-            ...findingAgentInputs.disable,
-            ...findingType,
-            metadata: {
-              agentId: mockAgentId,
-              enabled: 'false',
-              permission: '1',
-            },
-          }),
-        ]);
-      });
+      expect(findings).toEqual([
+        Finding.fromObject({
+          ...findingAgentInputs.disable,
+          ...findingType,
+          metadata: {
+            agentId: mockAgentId,
+            enabled: 'false',
+            permission: '1',
+          },
+        }),
+      ]);
     });
   });
 });
